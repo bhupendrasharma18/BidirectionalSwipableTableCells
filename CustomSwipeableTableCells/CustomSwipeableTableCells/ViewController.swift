cc30ac582e128d0b70c15b55172742f957ae9b39
Bhupendra <bhupendrasharma18@gmail.com>
2020-05-07 20:54:51 +0530

    Project setup

---
+++ CustomSwipeableTableCells/CustomSwipeableTableCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomSwipeableTableCells
+//
+//  Created by Bhupendra Sharma on 07/05/20.
+//  Copyright © 2020 Bhupendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
